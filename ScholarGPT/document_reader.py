c01d3d81fa7f821fdc3658c74ca49d47fd558c89
SparshRastogi <75373475+SparshRastogi@users.noreply.github.com>
2023-07-25 09:08:47 -0700

    Update document_reader.py
    Added function for uncovering the format of given text

--- ScholarGPT/document_reader.py
+++ ScholarGPT/document_reader.py
@@ -6,6 +6,9 @@ pip install numpy '''
 
 
 import fitz
+import pandas as pd
+import re
+from unidecode import unidecode
 
 
 def block_reader(file_path):
@@ -18,3 +21,28 @@ def block_reader(file_path):
       block_dict[page_num] = block 
       page_num += 1 
   return block_dict
+
+
+
+
+def format_unpacker(block_dict):
+  rows = []
+  for page_num, blocks in block_dict.items():
+    for block in blocks:
+        if block['type'] == 0:
+            for line in block['lines']:
+                for span in line['spans']:
+                    xmin, ymin, xmax, ymax = list(span['bbox'])
+                    font_size = span['size']
+                    text = unidecode(span['text'])
+                    span_font = span['font']
+                    is_upper = False
+                    is_bold = False
+                    if "bold" in span_font.lower():
+                        is_bold = True
+                    if re.sub("[\(\[].*?[\)\]]", "", text).isupper():
+                        is_upper = True
+                    if text.replace(" ","") !=  "":
+                        rows.append((xmin, ymin, xmax, ymax, text, is_upper, is_bold, span_font, font_size))
+  spanning = pd.DataFrame(rows, columns=['xmin','ymin','xmax','ymax', 'text', 'is_upper','is_bold','span_font', 'font_size'])
+  return spanning
