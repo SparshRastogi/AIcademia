c78613f0276680745c2936abf9c7aad32dc2732a
SparshRastogi <75373475+SparshRastogi@users.noreply.github.com>
2023-08-04 23:28:36 +0530

    Update LLM.py

--- ScholarGPT/Langchain/LLM.py
+++ ScholarGPT/Langchain/LLM.py
@@ -1,20 +1,17 @@
-from langchain.llms import LlamaCpp
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from huggingface_hub import hf_hub_download
-from langchain.chains.question_answering import load_qa_chain
-from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import ElasticVectorSearch,Pinecone,Weaviate,FAISS
+from langchain.document_loaders import PyPDFLoader,DirectoryLoader
+from langchain.llms import CTransformers
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
 
 
 text_splitter = CharacterTextSplitter(
     separator = "\n",
     chunk_size = 1000,
     chunk_overlap  = 100,
-    length_function = len,
-)
+    length_function = len,)
 texts = text_splitter.split_text(raw_text)
 llm = LlamaCpp(
     model_path=model_path,
@@ -23,22 +20,9 @@ llm = LlamaCpp(
     n_batch=n_batch,
     callback_manager=callback_manager,
     n_ctx=1536,
-    verbose=False,
-)
+    verbose=False,)
 
 
 
-n_gpu_layers = 40  # Change this value based on your model and your GPU VRAM pool.
-n_batch = 256  # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
 
-# Loading model,
-llm = LlamaCpp(
-    model_path=model_path,
-    max_tokens=256,
-    n_gpu_layers=n_gpu_layers,
-    n_batch=n_batch,
-    callback_manager=callback_manager,
-    n_ctx=1536,
-    verbose=False,
-)
 chain = load_qa_chain(llm, chain_type="stuff")
