977a5995bb0a48e41258b8ff348eed96c501935c
SparshRastogi <75373475+SparshRastogi@users.noreply.github.com>
2023-08-05 20:27:41 +0530

    Update LLM.py

--- ScholarGPT/Langchain/LLM.py
+++ ScholarGPT/Langchain/LLM.py
@@ -26,3 +26,36 @@ llm = LlamaCpp(
 
 
 chain = load_qa_chain(llm, chain_type="stuff")
+loader = DirectoryLoader(source_path,glob = "*.pdf",loader_cls = PyPDFLoader)
+
+
+
+data = loader.load()
+
+
+
+splitter = RecursiveCharacterTextSplitter(chunk_size = 500,chunk_overlap = 100)
+
+
+
+chunks = splitter.split_documents(data)
+
+
+
+embeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
+
+
+
+
+db = FAISS.from_documents(chunks,embeddings)
+
+db.save_local(db_path)
+custom_prompt_template = """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
