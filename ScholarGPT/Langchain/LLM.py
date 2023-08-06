66ad63e3e1afe7ce8e890a244803cd3b44d7ec6f
SparshRastogi <75373475+SparshRastogi@users.noreply.github.com>
2023-08-06 22:47:12 +0530

    Update LLM.py

--- ScholarGPT/Langchain/LLM.py
+++ ScholarGPT/Langchain/LLM.py
@@ -7,13 +7,17 @@ from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 
 
-text_splitter = CharacterTextSplitter(
+def splitter(raw_text):
+    text_splitter = CharacterTextSplitter(
     separator = "\n",
     chunk_size = 1000,
     chunk_overlap  = 100,
     length_function = len,)
-texts = text_splitter.split_text(raw_text)
-llm = LlamaCpp(
+    texts = text_splitter.split_text(raw_text)
+    return texts
+
+def model():
+    llm = LlamaCpp(
     model_path=model_path,
     max_tokens=256,
     n_gpu_layers=n_gpu_layers,
@@ -22,40 +26,3 @@ llm = LlamaCpp(
     n_ctx=1536,
     verbose=False,)
 
-
-
-
-chain = load_qa_chain(llm, chain_type="stuff")
-loader = DirectoryLoader(source_path,glob = "*.pdf",loader_cls = PyPDFLoader)
-
-
-
-data = loader.load()
-
-
-
-splitter = RecursiveCharacterTextSplitter(chunk_size = 500,chunk_overlap = 100)
-
-
-
-chunks = splitter.split_documents(data)
-
-
-
-embeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
-
-
-
-
-db = FAISS.from_documents(chunks,embeddings)
-
-db.save_local(db_path)
-custom_prompt_template = """Use the following pieces of information to answer the user's question.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-
-Context: {context}
-Question: {question}
-
-Only return the helpful answer below and nothing else.
-Helpful answer:
-"""
